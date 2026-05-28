a6d87d8c145eab1653edce2d1f3444ad78e4c24d
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-05-28 15:02:42 +0000

    Generate of Family;unit output in place.

--- libraries/hec-legacy-units/src/main/java/mil/army/usace/hec/units/GenUnitDefinitions.java
+++ libraries/hec-legacy-units/src/main/java/mil/army/usace/hec/units/GenUnitDefinitions.java
@@ -1,19 +1,27 @@
 package mil.army.usace.hec.units;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
-import mil.army.usace.hec.units.Parameters;
+import cwms.units.Loader;
+import cwms.units.Unit;
+
 
 public final class GenUnitDefinitions {
     public static void main(String[] args) {
         System.out.println("Hello wil use " + args[0]);
         final var outputDir = new File(args[0]);
 
+
+
         try {
-        generateParameters(outputDir);
+            final var loader = new Loader();
+            final var parameters = Parameters.load();
+
+            generateParameters(outputDir, parameters);
+            generateUnitConversions(outputDir, parameters, loader);
         } catch (IOException ex) {
             System.err.println("Unable to write unit information.");
             ex.printStackTrace(System.err);
@@ -21,16 +29,14 @@ public final class GenUnitDefinitions {
 
     }
 
-    private static void generateParameters(File outputDir) throws IOException {
+    private static void generateParameters(File outputDir, List<Parameter> parameters) throws IOException {
         try (PrintWriter writer = new PrintWriter(new File(outputDir, "parameters_units.def"))) {
             writer.println();
             writer.println("=PARAMETER/UNIT");
             writer.println("#position=100");
             writer.println();
 
-            var parameters = Parameters.load();
 
-            
 
             int maxParamNameLen = 0;
             int maxUnitAbbrLen = 0;
@@ -50,5 +56,70 @@ public final class GenUnitDefinitions {
                 writer.println(String.format(fmt, parameter.id(), parameter.siUnit(), parameter.usUnit()));
             }
         }
+    }
+
+
+    private static void generateUnitConversions(File outputDir, List<Parameter> parameters, Loader loader) throws IOException {
+        try(var writer = new PrintWriter(new File(outputDir, "unitConversions.def"))) {
+            writer.println("// Generated from hec-units" );
+            writer.println("// UNIT DEFINITIONS");
+            writer.println("//  UnitSystem;UnitName;UnitAliases...;...;");
+
+
+            final var units = loader.getUnits().values();
+            final var abstractParameters = loader.getAbstractParameters();
+
+            var systems = units.stream()
+                               .map(u -> u.getSystem())
+                               .distinct()
+                               .sorted()
+                               .toList();
+            var allSystems = systems.stream()
+                                    .filter(s -> s != null)
+                                    .filter(s -> !"NULL".equalsIgnoreCase(s))
+                                    .toList();
+
+
+            for (var abstractParameter: abstractParameters) {
+                writer.println();
+
+                writer.println("// " + abstractParameter);
+
+
+                var currentUnits = units.stream()
+                                        .filter(u -> u.getAbstractParameter().equals(abstractParameter))
+                                        .toList();
+
+                for (var system: systems) {
+                    var systemUnits = currentUnits.stream()
+                                                  .filter(u -> u.getSystem().equals(system))
+                                                  .toList();
+                    for (var unit: systemUnits) {
+                        if (allSystems.contains(system)) {
+                            renderUnit(writer, system, unit);
+                        } else {
+                            for (var namedSystem: allSystems) {
+                                renderUnit(writer, namedSystem, unit);
+                            }
+                        }
+                    }
+                    writer.println();
+                }
+
+                writer.println("// " + abstractParameter + " Conversions");
+
+                writer.println();
+            }
+
+        }
+    }
+
+
+    private static void renderUnit(PrintWriter writer, String system, Unit unit) {
+        writer.print(system + ";" + unit.getAbbreviation());
+        for (var alias: unit.getAliases()) {
+            writer.print(";" + alias);
+        }
+        writer.println();
     }
 }
